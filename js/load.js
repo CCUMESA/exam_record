b848c27ee9fa380a8251c672232485404e57ff39
david11014 <david11014@gmail.com>
2015-04-28 16:31:17 +0800

    更改檔案架構，壓縮容量

--- js/load.js
+++ js/load.js
@@ -1,12 +1,5 @@
-﻿var data=new Array();
-data[0]=[102,0,1,1]; //[年分,上(0)下(1)學期,第幾次,有幾頁]
-data[1]=[102,1,2,1];
-data[2]=[103,0,1,1];
-data[3]=[103,1,1,1];
-data[4]=[104,0,1,3];
-var mF = 0,nF = 0;
-var index = data.length-1;			
-var subject = "微積分";
+﻿var mF = 0,nF = 0; //行動裝置menu改寫旗標
+var index = data.length-1; //當前年份索引值
 
 $(window).load(function(){
 	Load();
@@ -33,11 +26,8 @@ function Load(){
 	}
 	else
 	{
-		addThingW();
-		
+		addThingW();	
 	}
-		
-	
 	show();	
 }
 
@@ -45,13 +35,13 @@ function addThingM(){
 	$(nav).ready(function() {
 		var StyleHead = '<a class="link depth-1" href="javascript:ChangeYear(';
 		var StyleMiddle = ')" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"><span class="indent-1"></span>';
-		//var nav = $('[data-action="navList"]>nav>[href="##"]');
 		var nav = $('[data-action="navList"]>nav>a:eq(1)');
-		//alert("aa");
-		for (var i =data.length -1; i >=0 ; i--)
+		var str = "";
+		for (var i = 0; i < data.length  ; i++)
 		{
-			nav.after(StyleHead + i + StyleMiddle + data[i][0] + Se(data[i][1]) + Ti(data[i][2]) +  '</a>');
+			str = str + StyleHead + i + StyleMiddle + data[i][0] + Se(data[i][1]) + Ti(data[i][2]) +  '</a>';
 		}
+		nav.after(str);
 	});
 }
 
@@ -61,25 +51,23 @@ function addThingW(){
 	var StyleMiddle1 = ')" touchstart="ChangeYear(';
 	var StyleMiddle2 = ')"> ';
 	var StyleFoot = '</a></li>';
-	var nav = $('.droptron:eq(0)>li[style="white-space: nowrap"]');
 	$(".y").empty(); //清空menu內容
-	
+	var str = "";
 	for(var i =0; i < data.length; i++)
 	{
-		var str = StyleHead + i + StyleMiddle1 + i + StyleMiddle2 + data[i][0] + Se(data[i][1]) + Ti(data[i][2]) + StyleFoot;
-		$(".y").append(str);
+		str = str + StyleHead + i + StyleMiddle1 + i + StyleMiddle2 + data[i][0] + Se(data[i][1]) + Ti(data[i][2]) + StyleFoot;
 	}
+	$(".y").append(str);
 }
 function ChangeYear(i){
 	index = i;
 	show();
 	$("html,body").animate({scrollTop: $("#main").offset().top-160}, 1000);
-
 }
 			
 function Se(s){	return s ==0?'上':'下';	}
 function Ti(t){
-				
+
 	switch(t){
 		case 1:
 			a = "第一次";
@@ -96,7 +84,7 @@ function Ti(t){
 	}
 	return a;
 }
-			
+
 function show() {
 	$(".content>section").empty();
 	var str = "";
@@ -106,7 +94,7 @@ function show() {
 	}
 	$(".content>section").append(str);
 	$("#year_show").empty();
-	$("#year_show").append(data[index][0] + '年' + Se(data[i][1]) + Ti(data[i][2]) + '段考');
+	$("#year_show").append(data[index][0] + '年' + Se(data[index][1]) + Ti(data[index][2]) + '段考');
 	
 }
 

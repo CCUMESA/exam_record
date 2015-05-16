9ae4b66bb4f909c0cf974002b0d729866ae4307a
david11014 <david11014@gmail.com>
2015-05-16 10:49:35 +0800

    結構修改

--- js/load.js
+++ js/load.js
@@ -2,6 +2,15 @@
 var index = data.length-1; //當前年份索引值
 
 $(window).load(function(){
+	Load();
+});	
+
+skel.on('change', function() {
+	Load();
+});
+
+function Load(){
+
 	if (skel.isActive('narrower'))
 	{
 		$('.toggle').bind('touchend',chnF);
@@ -15,11 +24,7 @@ $(window).load(function(){
 	else{addThingW();}
 	
 	show();	
-});	
-
-skel.on('change', function() {
-	Load();
-});
+}
 
 function chnF(){
 	if(nF == 0)
@@ -102,19 +107,33 @@ function title_str(i)
 	return str;
 }
 
+function format_num(i)
+{
+	if((i/10) < 1)
+		return "000" + i;
+	else if((i/100) < 1)
+		return "00" + i;
+	else if((i/1000) < 1)
+		return "0" + i;
+	else
+		return "" + i ;
+}
+
 function show() {
 	$(".content>section").empty();
 	//資料庫網址
 	var data_url = "http://ccumesa.github.io/exam_data/";
-	
 	//data_url = "/data/";  //本機端資料庫測試用
-	var dw = data_url  + grade + '/' + subject + '/'+ data[index][0] + '-' + data[index][1] + '-' + data[index][2] + '.pdf';
+	//資料夾路徑
+	var folder_url = grade + '/' + subject + '/' + data[index][0] + '-' + data[index][1] + '-' + data[index][2] + '/' ;
 	
-	var str = '<p><a href="' + dw + '">PDF下載</a></p>';
+	var dw = data_url  + folder_url + data[index][0] + '-' + data[index][1] + '-' + data[index][2] + '.pdf';
+	
+	var str = '<p><a href="' + dw + '" target ="_blank">PDF下載</a></p>';
 	
 	for(var i =1;i<=data[index][3];i++){
-		var url = grade + '/' + subject + '/'+ data[index][0] + '-' + data[index][1] + '-' + data[index][2] + '-' + i +'.jpg';
-		str = str + '\n' + '<a class="image featured"><img src="' + data_url + url + ' "alt="" /></a>';
+		var url =data_url + folder_url + format_num(i) +'.jpg';
+		str = str + '\n' + '<a class="image featured"><img src="' + url + ' "alt="" /></a>';
 	}
 	$(".content>section").append(str);
 	$("#year_show").empty();
